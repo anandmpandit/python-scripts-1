5052aba53dc5731e4e44a2bd9cd643ae780ba3ea
Gaurav Pandey <developer.gauravpandey@gmail.com>
2021-03-20 10:46:22 +0530

    Add files via upload

---
+++ age_finder/code.py
@@ -0,0 +1,6 @@
+print("This is Age Finder Python Program")
+print("Created by @gauravpandey_")
+
+birth_year = input('Birth Year : ')
+age = 2021 - int(birth_year)
+print("You are " + str(age) + " Years Old")
---
+++ odd_or_even/code.py
@@ -0,0 +1,10 @@
+print("This is a Odd or Even Finder Python Program")
+print("Created by @gauravpandey_")
+
+import random
+number = int(input('Which Number you are thinking: '))
+
+if (number % 2) == 0:
+    print("This Number is divisible by 2 \nGreat..!!")
+else:
+    print('This Number is not divisible by 2 \nGreat..!!')
