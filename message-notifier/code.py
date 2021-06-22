b9d0cf5b67008e170e7f454d7ec7196a7531c098
Gaurav Pandey <developer.gauravpandey@gmail.com>
2021-06-22 13:41:41 +0530

    update

---
+++ message-notifier/code.py
@@ -0,0 +1,10 @@
+print("Created by Gaurav Pandey")
+
+from plyer import notification
+
+notification.notify( 
+	title = "Hello World!", 
+	message= "My test Message!", 
+	# displaying time 
+  timeout=10 
+)
